8bfd1490f39372616cae41d22f02703738bf7077
por <155146687+por1a@users.noreply.github.com>
2024-07-04 19:56:08 +0700

    fixed spelling

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 function getComputerChoice() {
     let computerChoice = "Unchosen"
+
+    // randomly generate a choice
     randomNum = Math.floor((Math.random() * 3) + 1);
     if (randomNum === 1) {
         computerChoice = "rock";
@@ -8,17 +10,19 @@ function getComputerChoice() {
         computerChoice = "paper";
 
     } else if (randomNum === 3) {
-        computerChoice = "scissor";
+        computerChoice = "scissors";
     }
     return computerChoice;
 }
 
 function getHumanChoice() {
+
+    // Asks human for choice, reask if not rock, paper or scissors
     let condition = 1;
     let humanChoice;
     while (condition === 1) {
-        humanChoice = prompt("Rock paper or scissor?: ").toLowerCase().trim();
-        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissor") {
+        humanChoice = prompt("Rock paper or scissors?: ").toLowerCase().trim();
+        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
             condition = 0;
         } else {
             alert("Input is not rock, paper or scissors.")
@@ -31,6 +35,8 @@ let humanScore = 0, computerScore = 0;
 
 function playRound(computerChoice, humanChoice) {
     let endMessage = "";
+
+    // Compare choice between human and computer
     if (humanChoice === computerChoice) {
         endMessage = `Computer chose ${computerChoice}! It was a tie!`;
         humanScore += 0.5;
@@ -38,10 +44,10 @@ function playRound(computerChoice, humanChoice) {
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         endMessage = `Computer wins with ${computerChoice}!`;
         computerScore++;
-    } else if (humanChoice === "paper" && computerChoice === "scissor") {
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
         endMessage = `Computer wins with ${computerChoice}!`; 
         computerScore++;
-    } else if (humanChoice === "scissor" && computerChoice === "rock") {
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
         endMessage = `Computer wins with ${computerChoice}!`;
         computerScore++;
     } else {
@@ -52,12 +58,16 @@ function playRound(computerChoice, humanChoice) {
 }   
 
 function playGame() { 
+
+    // Loop until 5 rounds are reached, output results
     for (let rounds = 0; rounds < 5; rounds++) {
         let result = playRound(getComputerChoice(), getHumanChoice()); 
         alert(result);
         console.log(result)
         
     } 
+    
+    // Compare score, check for winner
     let winner;
     if (humanScore > computerScore) {
         winner = "Human";
@@ -66,13 +76,14 @@ function playGame() {
     } else {
         winner = "It was a tie!"
     }
-    winnerMessage = `Human score: ${humanScore}\nComputer score ${computerScore}\nWinner is ${winner}`
+
+    winnerMessage = `Human score: ${humanScore}\nComputer score: ${computerScore}\nWinner is ${winner}`
 
     return winnerMessage 
 
 }
 
-
+// A win is 1 point. Tie is 0.5 each
 game = playGame();
 console.log(game)
 alert(game)
